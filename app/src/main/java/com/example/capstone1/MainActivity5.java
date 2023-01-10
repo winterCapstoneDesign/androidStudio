b5e1133b9e7593f3c71cdafc22f83dcebb9c6441
Sun Young Bae <tjsdud6677@gmail.com>
2023-01-10 10:26:41 +0900

    log code update

--- app/src/main/java/com/example/capstone1/MainActivity5.java
+++ app/src/main/java/com/example/capstone1/MainActivity5.java
@@ -1,12 +1,19 @@
 package com.example.capstone1;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -27,30 +34,38 @@ public class MainActivity5 extends AppCompatActivity {
     DatabaseReference databaseReference;
 
     ListView listView;
+    Button goMainbtn;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
 
+        goMainbtn = findViewById(R.id.log_go_main);
+        goMainbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity5.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         listView = findViewById(R.id.lv_LogList);
-        adapter = new ArrayAdapter<String>(this, R.layout.activity_main5, arrayList);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference("QR_lock").child("date");
-        getValue();
-    }
+        databaseReference = firebaseDatabase.getReference().child("QR_lock");
 
-    private void getValue() {
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        databaseReference.child("date").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                arrayList.clear();
-                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    String sValue = dataSnapshot.child(Integer.toString(1)).getValue(String.class);
-                    arrayList.add(sValue);
+                for (DataSnapshot logSnapshot : snapshot.getChildren()) {
+                    String str = logSnapshot.child("1").getValue(String.class);
+                    arrayList.add(str);
                 }
-                listView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
             }
 
             @Override
